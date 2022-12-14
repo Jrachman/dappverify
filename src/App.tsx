17b1743709e904e8ba05822cedfae07595054a71
Julian Rachman <rachmanjulian8@gmail.com>
2022-12-14 02:10:13 -0800

    added chrome fetch url and parse for root domain

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,30 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+function extractRootDomain(u: string) {
+  const domain = new URL(u).hostname;
+  const elems = domain.split('.');
+  const iMax = elems.length - 1;
+    
+  const isSecondLevel = elems.length >= 3 && (elems[iMax] + elems[iMax - 1]).length <= 5;
+  return elems.splice( isSecondLevel ? -3 : -2 ).join('.');	
+}
+
 function App() {
+
+  const [url, setUrl] = useState<string | undefined>(undefined);
+  
+  useEffect(() => {
+    chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
+      setUrl(extractRootDomain(tabs[0].url as string));
+      // use `url` here inside the callback because it's asynchronous!
+    });  
+  }, []); 
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>{url}</p>
     </div>
   );
 }
